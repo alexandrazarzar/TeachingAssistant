17d68ddda82653807ad1189d717312bfdda39b72
Alexandra Zarzar <alexandra@MacBook-Pro-de-Alexandra.local>
2021-08-25 00:20:46 -0300

    refatoracao de atualizarAluno

--- ta-gui/src/app/alunos.component.ts
+++ ta-gui/src/app/alunos.component.ts
@@ -8,6 +8,7 @@ import { AlunoService } from "./aluno.service";
   templateUrl: "./alunos.component.html",
   styleUrls: ["./alunos.component.css"],
 })
+
 export class AlunosComponent implements OnInit, OnDestroy {
   aluno: Aluno = { nome: "", cpf: "", email: "" };
   errorMessage: string = "";
@@ -47,17 +48,13 @@ export class AlunosComponent implements OnInit, OnDestroy {
   }
 
   atualizarAluno(aluno: Aluno): void {
-    let nome  = (document.getElementById("new_nome_input") as HTMLInputElement).value;
-    let cpf   = (document.getElementById("new_cpf_input") as HTMLInputElement).value;
-    let email = (document.getElementById("new_email_input") as HTMLInputElement).value;
-    let alunoNew: Aluno = new Aluno(nome, cpf, email);
+    this.errorMessage = "";
+    let alunoNew: Aluno = this.inputDeAtualizacao("new_nome_input", "new_cpf_input", "new_email_input");
 
     this.alunoService.atualizar(alunoNew, this.cpfAluno).subscribe({
       next: (alunoNew) => {
         this.cpfAluno = "";
-        aluno.nome = nome;
-        aluno.cpf = cpf;
-        aluno.email = email;
+        this.copiar(aluno, alunoNew);
       },
       error: (error) => {
         this.errorMessage = error.message;
@@ -70,4 +67,17 @@ export class AlunosComponent implements OnInit, OnDestroy {
     this.cpfAluno = aluno.cpf as string;
   }
 
+  inputDeAtualizacao(inputNome, inputCpf, inputEmail: string): Aluno {
+    let nome  = (document.getElementById(inputNome) as HTMLInputElement).value;
+    let cpf   = (document.getElementById(inputCpf) as HTMLInputElement).value;
+    let email = (document.getElementById(inputEmail) as HTMLInputElement).value;
+    let aluno: Aluno = new Aluno(nome, cpf, email);
+    return aluno
+  }
+
+  copiar(alunoInicial, alunoFinal: Aluno): void{
+    alunoInicial.nome = alunoFinal.nome;
+    alunoInicial.cpf = alunoFinal.cpf;
+    alunoInicial.email = alunoFinal.email;
+  }
 }
